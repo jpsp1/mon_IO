7c0e0860dca377e57300808695512e0c52f1e04d
Ubuntu <joao.pagaime@gmail.com>
2025-04-11 14:48:44 +0100

    updates a Fri Apr 11 14:48:43 WEST 2025

--- latencia_io.sh
+++ latencia_io.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-L=/root/scripts/latencia_io.data
+L=latencia_io.data.`hostname`
 if [ $(stat -c%s "$L") -gt $((10 * 1024 * 1024)) ]; then
     mv $L $L.old
 fi
 sleep $((RANDOM % 10 + 1))
-timeout 30 ioping -c 10 /root/scripts 2>&1 > $L
+timeout 30 ioping -c 10 . 2>&1 > $L
 read avgg avgg_u mdevv mdevv_u <<< $(grep mdev $L | awk '{print $6,$7,$12,$13}' | tr '.' ',')
 d=$(date +"%Y-%m-%d %H:%M")
 printf "%s;%s;%s;%s;%s\n" "$d" $avgg $avgg_u $mdevv $mdevv_u >> $L.historico
-cp $L.historico /tmp/
-
-
