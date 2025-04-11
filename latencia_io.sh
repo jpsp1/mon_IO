841168337acb91e3317b1fe0fe4bd34b41ff7236
Ubuntu <joao.pagaime@gmail.com>
2025-04-11 16:45:18 +0100

    chimaera2-serv: updates a Fri Apr 11 16:45:18 WEST 2025

--- latencia_io.sh
+++ latencia_io.sh
@@ -5,7 +5,8 @@ sleep $((RANDOM % 10 + 1))
 timeout 30 ioping -c 10 . 2>&1 > $L
 read avgg avgg_u mdevv mdevv_u <<< $(grep mdev $L | awk '{print $6,$7,$12,$13}' | tr '.' ',')
 d=$(date +"%Y-%m-%d %H:%M")
-LH=$L.historico.`hostname`
+HN=`hostname`
+LH=$L.historico.$HN
 printf "%s;%s;%s;%s;%s\n" "$d" $avgg $avgg_u $mdevv $mdevv_u >> $LH
 if [ $(stat -c%s "$LH") -gt $((1 * 1024 * 1024)) ]; then
     mv $LH $LH.old
@@ -15,6 +16,6 @@ sleep $((RANDOM % 10 + 1))
 #Normally, git pull = git fetch + git merge.
 git pull --rebase
 d=`date`
-git commit -m "updates a $d" -a
+git commit -m "$HN: updates a $d" -a
 git push -u origin main
 
