cfa29744aac0a05327d412c03d722cb9cf6e8bba
AIA-jpsp <joao.pagaime@gmail.com>
2025-10-16 11:05:50 +0100

    updates a Thu Oct 16 11:05:50 WEST 2025

--- latencia_io.sh
+++ latencia_io.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
+
+################# outputs
+# --- ficheiro  latencia_io.data.historico.<hostname>
+#...
+#2025-10-16 10:55;1,03;ms;120,9;us;2782;sect_R_sec;74;sect_W_sec
+#2025-10-16 11:00;1,12;ms;132,7;us;2808;sect_R_sec;4120;sect_W_sec
+#colunas
+# data/hora
+# tempo médio READ
+# tempo médio WRITE
+# IOS- READ 
+# IOS- WRITE
+#
+# --- ficheiro  latencia_io.data.historico.<hostname>.carga 
+#...
+#2025-10-16 10:55;1760608519;1571548123;75362900
+#2025-10-16 11:00;1760608820;1572393483;76603048
+#colunas
+# data/hora
+#    sectors read, contador
+#    sectors written, contador
+
 cd /usr/local/mon_IO
 L=latencia_io.data
 sleep $((RANDOM % 10 + 1))
